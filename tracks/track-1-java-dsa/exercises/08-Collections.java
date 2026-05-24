a5224d1e0b3145cd517ec84555fa4f9f88e6536c
Stacey Spears <stacey.spears@mykenzie.snhu.edu>
2026-05-24 03:22:52 -0500

    Fix Track 1 exercise class declarations

--- tracks/track-1-java-dsa/exercises/08-Collections.java
+++ tracks/track-1-java-dsa/exercises/08-Collections.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class Collections {
+class Collections {
     public static void main(String[] args) {
         // --- ArrayList ---
 
